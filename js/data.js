2608d400410c6173e5181a4df04e58644e0b8b58
hezicg-apps <hezicg.work@gmail.com>
2026-03-21 08:04:39 +0200

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,18 @@
+const courseData = {
+    "Magical": [
+        {
+            unit: "Unit 1",
+            parts: [
+                { name: "Part 1", words: "Apple - תפוח\nBanana - בננה" },
+                { name: "Part 2", words: "Dog - כלב\nCat - חתול" }
+            ]
+        },
+        // ... המשך לכל היחידות בגיליון
+    ],
+    "Legendary": [
+        // ... נתונים מהגיליון של Legendary
+    ],
+    "Epic": [
+        // ... נתונים מהגיליון של Epic
+    ]
+};
